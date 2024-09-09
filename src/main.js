22b9c91a65d4d4a99a0de3bc0ebaa5cdcd251051
Klaus Karkia <klaus@karkia.me>
2024-09-09 20:15:09 +0300

    Updated serviceworker

--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
 import './assets/main.css'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope)
-      })
-      .catch(error => {
-        console.log('Service Worker registration failed:', error)
-      })
-  })
-}
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
