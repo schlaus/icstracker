27076bf5227e24f0c10c7c7950214bcbe3c44ef8
Klaus Karkia <klaus@karkia.me>
2024-09-09 16:36:02 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import './assets/main.css'
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope)
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error)
+      })
+  })
+}
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
